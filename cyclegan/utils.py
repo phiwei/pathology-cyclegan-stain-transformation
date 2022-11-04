0e04de03043384f9965a7a1ae6467975046efd27
phiwei@mac <philippe.weitz@ki.se>
2022-11-04 16:32:23 +0100

    use yaml.safe_load

--- cyclegan/utils.py
+++ cyclegan/utils.py
@@ -18,7 +18,7 @@ IMG_SIZE = (512, 512)
 
 def get_config_from_yaml(config_path):
     with open(file=config_path, mode='r') as param_file:
-        parameters = yaml.load(stream=param_file)
+        parameters = yaml.safe_load(stream=param_file)
     return parameters['model'], parameters['training']
 
 
