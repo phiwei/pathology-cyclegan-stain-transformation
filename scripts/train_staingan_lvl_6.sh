ae843f4229a473e7d9a14f27398bb52297fffa2c
phiwei@chime1 <philippe.weitz@ki.se>
2022-11-04 16:31:18 +0100

    move scripts to main folder

--- scripts/train_staingan_lvl_6.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python main.py --run_name staingan6 --output_dir /mnt/hdd8tb/phiwei/staingan/ --param_file_path /home/phiwei/repos/pathology-cyclegan-stain-transformation/config/config_cyclegan.yaml --albumentations_path /home/phiwei/repos/pathology-cyclegan-stain-transformation/config/config_albumentations.yml --data_file_path /mnt/ssd2tb/phiwei/acrobat_train_low_res/6/
