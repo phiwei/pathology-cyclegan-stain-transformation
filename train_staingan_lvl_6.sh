f79a6a1308ba168ccc7a1b4b159a5f116d441e44
phiwei@mac <philippe.weitz@ki.se>
2022-11-05 12:09:26 +0100

    first run on lvl 6, then 5

--- train_staingan_lvl_6.sh
+++ train_staingan_lvl_6.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 python main.py --run_name staingan6 --output_dir /mnt/hdd8tb/phiwei/staingan/ --param_file_path /home/phiwei/repos/pathology-cyclegan-stain-transformation/config/config_cyclegan_mod.yml --albumentations_path /home/phiwei/repos/pathology-cyclegan-stain-transformation/config/config_albumentations.yml --data_file_path /mnt/ssd2tb/phiwei/acrobat_train_low_res/6/
+sleep 5m
+python main.py --run_name staingan5 --output_dir /mnt/hdd8tb/phiwei/staingan/ --param_file_path /home/phiwei/repos/pathology-cyclegan-stain-transformation/config/config_cyclegan_mod.yml --albumentations_path /home/phiwei/repos/pathology-cyclegan-stain-transformation/config/config_albumentations.yml --data_file_path /mnt/ssd2tb/phiwei/acrobat_train_low_res/5/
